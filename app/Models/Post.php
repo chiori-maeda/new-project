8bc9908414a8f8a4c270c0e4997a6f3ef50124f6
8524116nana <nanamax1205@gmail.com>
2025-11-26 16:48:29 +0900

    indexとpostとcommentのテーブルとそれに相対するmodel

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model{
+    public function user() {
+       return $this->belongsTo(User::class);
+    }
+
+    public function comments() {
+       return $this->hasMany(Comment::class);
+    }
+}
 
 
 ?>
